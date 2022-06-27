a345a94ec0dc0350cff781b5eaf974da50641a0b
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-27 17:44:05 -0400

    feat: get notes and calculate the mean

--- app/src/main/java/com/enriqueizel/gradecalculator/MainActivity.kt
+++ app/src/main/java/com/enriqueizel/gradecalculator/MainActivity.kt
@@ -11,5 +11,15 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    binding.btnCalculate.setOnClickListener {
+      val note1 = binding.schoolGrade1.text.toString().toDouble()
+      val note2 = binding.schoolGrade2.text.toString().toDouble()
+      val note3 = binding.schoolGrade3.text.toString().toDouble()
+      val note4 = binding.schoolGrade4.text.toString().toDouble()
+      val absences = binding.absences.text.toString().toInt()
+
+      val meanNotes = (note1 + note2 + note3 + note4) / 4
+    }
   }
 }
